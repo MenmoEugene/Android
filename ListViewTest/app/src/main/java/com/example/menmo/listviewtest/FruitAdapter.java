5c88df468f38dab1b25b05f7775b99caada21aa8
MenmoEugene <menmo_o@163.com>
2018-02-07 21:47:20 +0800

    updata

--- ListViewTest/app/src/main/java/com/example/menmo/listviewtest/FruitAdapter.java
+++ ListViewTest/app/src/main/java/com/example/menmo/listviewtest/FruitAdapter.java
@@ -19,17 +19,31 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     private int resourceId;
 
     public FruitAdapter(Context context, int textViewResourceId, List<Fruit> objects) {
-        super(context, textViewResourceId);
+        super(context, textViewResourceId,objects);
         resourceId = textViewResourceId;
     }
 
     public View getView(int position, View convertView, ViewGroup parent) {
         Fruit fruit = getItem(position);//获取当前项的Fruit实例
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
-        TextView fruitName = (TextView) view.findViewById(R.id.fruid_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = (TextView) view.findViewById(R.id.fruid_name);
+            view.setTag(viewHolder);
+        }else{
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    private class ViewHolder {
+        public ImageView fruitImage;
+        public TextView fruitName;
     }
 }
--- ListViewTest/app/src/main/java/com/example/menmo/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/menmo/listviewtest/MainActivity.java
@@ -2,17 +2,17 @@ package com.example.menmo.listviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private String[] data ={"Apple","Banana","Orange","Watermelon","Pear",
-                    "Grape","Pineapple","Strawberry","Cherry","Mango","Jackfruit",
-                    "Litchi","Pawpaw","Carambola"};
     private List<Fruit> fruitList = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +22,17 @@ public class MainActivity extends AppCompatActivity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Fruit fruit = fruitList.get(position);
+            Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+//        new AdapterView<?>parent,View view, int position,long id){
+//            Fruit fruit = fruitList.get(position);
+//            Toast.makeText(MainActivity.this,fruit.getName(),Toast.LENGTH_SHORT).show();
+//        }
     }
     private void initFruits(){
         for (int i =0 ;i < 2;i++){
