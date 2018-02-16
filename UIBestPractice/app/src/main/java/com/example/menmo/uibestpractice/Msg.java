c1b9cba236f26dd0c918509ba00b1a7c652fc88e
MenmoEugene <menmo_o@163.com>
2018-02-16 23:27:17 +0800

    updata

---
+++ UIBestPractice/app/src/main/java/com/example/menmo/uibestpractice/Msg.java
@@ -0,0 +1,25 @@
+package com.example.menmo.uibestpractice;
+
+/**
+ * Created by Administrator on 2018/2/16 0016.
+ */
+
+public class Msg {
+    public static final int TYPE_RECEIVED = 0;
+    public static final int TYPE_SENT = 1;
+    private String content;
+    private int type;
+
+    public Msg(String content, int type) {
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getType() {
+        return type;
+    }
+}
