9b6f52130d1da9788f363a7488927164d08eb3d8
MenmoEugene <menmo_o@163.com>
2018-02-18 18:44:09 +0800

    updata/FragmentTest

---
+++ FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.menmo.fragmenttest;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button =(Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+        replaceFragment(new RightFragment());
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:
+                replaceFragment(new AnotherRightFragment());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction =fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout,fragment);
+        transaction.commit();
+
+    }
+}
