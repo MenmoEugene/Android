b7d98600f607e4a4173d20bc4cb105d2ef32499c
MenmoEugene <menmo_o@163.com>
2018-02-19 17:14:20 +0800

    updata

--- FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/AnotherRightFragment.java
+++ FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/AnotherRightFragment.java
@@ -1,8 +1,7 @@
 package com.example.menmo.fragmenttest;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/MainActivity.java
@@ -1,26 +1,30 @@
 package com.example.menmo.fragmenttest;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
+
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button =(Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
         replaceFragment(new RightFragment());
+
     }
+
     @Override
-    public void onClick(View v){
-        switch (v.getId()){
+    public void onClick(View view) {
+        switch (view.getId()){
             case R.id.button:
                 replaceFragment(new AnotherRightFragment());
                 break;
@@ -28,12 +32,10 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
     }
-
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction =fragmentManager.beginTransaction();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.right_layout,fragment);
         transaction.commit();
-
     }
 }
