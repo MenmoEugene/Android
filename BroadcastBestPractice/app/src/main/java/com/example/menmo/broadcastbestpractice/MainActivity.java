e87012640c6804909e333ab79ff02f82ef28b778
MenmoEugene <menmo_o@163.com>
2018-02-27 18:59:57 +0800

    updata-garbageProject

--- BroadcastBestPractice/app/src/main/java/com/example/menmo/broadcastbestpractice/MainActivity.java
+++ BroadcastBestPractice/app/src/main/java/com/example/menmo/broadcastbestpractice/MainActivity.java
@@ -1,19 +1,12 @@
-package com.example.menmo.broadcasttest;
+package com.example.menmo.broadcastbestpractice;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         forceOffline.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent("com.example.menmo.broadcasttestpractice.FORCE_OFFLINE");
+                Intent intent = new Intent("com.example.menmo.broadcastbestpractice.FORCE_OFFLINE");
                 sendBroadcast(intent);
             }
         });
---
+++ BroadcastTest2/app/src/main/java/com/example/menmo/broadcasttest2/AnotherBroadcastReceiver.java
@@ -0,0 +1,17 @@
+package com.example.menmo.broadcasttest2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018/2/27 0027.
+ */
+
+public class AnotherBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in AnotherBroadcastReceiver", Toast.LENGTH_SHORT).show();
+    }
+}
