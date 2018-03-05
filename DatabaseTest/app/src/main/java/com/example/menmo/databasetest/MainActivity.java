586139328f624cba97da426b98d25dc76cc9cea2
MenmoEugene <menmo_o@163.com>
2018-03-05 16:00:59 +0800

    DatabaseTest、LitePalTest

--- DatabaseTest/app/src/main/java/com/example/menmo/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/menmo/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.menmo.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -41,6 +43,50 @@ public class MainActivity extends AppCompatActivity {
                 values.put("pages", 510);
                 values.put("price", 19.95);
                 db.insert("Book", null, values);//插入第二条数据链
+            }
+        });
+
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price", 10.99);
+                db.update("Book", values, "name = ?", new String[]{"The Da Vinci Code"});
+            }
+        });
+
+        Button deleteData = (Button) findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book", "pages > ?", new String[]{"500"});
+            }
+        });
+
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //查询Book 表中所有的数据
+                Cursor cursor = db.query("Book", null, null, null, null, null, null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        //遍历 Cursor 对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity", "book name is" + name);
+                        Log.d("MainActivity", "book author is" + author);
+                        Log.d("MainActivity", "book pages is" + pages);
+                        Log.d("MainActivity", "book price is" + price);
+                    } while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
---
+++ LitePalTest/app/src/main/java/com/example/menmo/litepaltest/Category.java
@@ -0,0 +1,23 @@
+package com.example.menmo.litepaltest;
+
+/**
+ * Created by Administrator on 2018/3/5 0005.
+ */
+
+public class Category {
+    private int id;
+    private String categoryName;
+    private int categoryCode;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+}
