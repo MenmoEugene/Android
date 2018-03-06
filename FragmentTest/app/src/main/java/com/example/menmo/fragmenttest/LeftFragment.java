93acf7991448cd71e5eddffc5311165d532d7934
MenmoEugene <menmo_o@163.com>
2018-03-06 18:13:12 +0800

    updata

--- FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/LeftFragment.java
+++ FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/LeftFragment.java
@@ -6,14 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by Administrator on 2018/2/18 0018.
- */
-
-public class LeftFragment extends Fragment{
+public class LeftFragment extends Fragment {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.left_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
         return view;
     }
 }
--- FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/MainActivity.java
+++ FragmentTest/app/src/main/java/com/example/menmo/fragmenttest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.menmo.fragmenttest;
 
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,7 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.button:
                 replaceFragment(new AnotherRightFragment());
                 break;
@@ -32,10 +31,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
     }
-    private void replaceFragment(Fragment fragment){
+
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.right_layout,fragment);
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);
         transaction.commit();
     }
 }
